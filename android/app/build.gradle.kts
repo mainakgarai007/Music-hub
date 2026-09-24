a973dfa304de5852efa96e1f70dfdd64b72e131e
Mainak Gorai <mainakgarai007@gmail.com>
2026-09-24 21:50:25 +0530

    Make Music-hub a separate Android app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ val keystorePropertiesFile = rootProject.file("key.properties")
 
 
 android {
-    namespace = "ls.bloomee.musicplayer"
+    namespace = "com.mainakgarai.musichub"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -27,7 +27,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "ls.bloomee.musicplayer"
+        applicationId = "com.mainakgarai.musichub"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
