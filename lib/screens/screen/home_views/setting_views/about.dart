8278030687c16c2523d9e55f3eeb2df0327b4ec4
Mainak Gorai <mainakgarai007@gmail.com>
2026-09-25 08:17:11 +0530

    Clean Music-hub About screen imports

--- lib/screens/screen/home_views/setting_views/about.dart
+++ lib/screens/screen/home_views/setting_views/about.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:Bloomee/l10n/app_localizations.dart';
-import 'package:icons_plus/icons_plus.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'dart:ui';
 import 'dart:math';
 
